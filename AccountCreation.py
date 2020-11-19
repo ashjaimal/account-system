7de46908afff376c8db7c0bcd390373b4a6ae72a
ashjaimal <62682183+ashjaimal@users.noreply.github.com>
2020-11-19 14:35:17 +0000

    AccountCreation Script
    started account creation script,

---
+++ AccountCreation.py
@@ -0,0 +1,12 @@
+def accountCreation():
+    userName = str
+    email = str
+    password = str
+    DOB = str
+
+
+    userName = input("Enter User Name: ")
+    emal = input("Enter your Email: ")
+    password = input("Enter a suitable Password: ")
+    DOB = input("Enter Date of Birth: ")
+
